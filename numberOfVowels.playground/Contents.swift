41193b0415153cdebab7c6df1ea7e2bedba937d6
Alexander Thompson <53663334+alext8900@users.noreply.github.com>
2019-09-09 20:33:14 -0500

    project done

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,26 @@
 import UIKit
 
-func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
-    
+func numberOfVowels(in string: String, yIsaVowel: Bool = false) -> Int {
+    var howManyVowels = 0
+    for vowel in string.lowercased() {
+        switch vowel {
+        case "a", "e", "i", "o", "u":
+            howManyVowels += 1
+        case "Y":
+            if yIsaVowel == true {
+                howManyVowels += 1
+            }
+        default:
+            break
+        
+        }
+        
+    }
+    return howManyVowels
 }
+//### Challenge
+
+
+
+//Write a function called `numberOfVowels(in string: String)` that returns the count of the total number of vowels in a string. Your solution should be case-insensitive, and allow for 'Y' to be included, or excluded from the count when calling the function.
+numberOfVowels(in: "How many apples can adam eat when he wants some apples?")
